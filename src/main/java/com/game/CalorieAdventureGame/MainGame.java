7e7c488c02c204fb8ccce14a6fc3e2a831b7ce72
ayeshamashiat <ayesha.mashiat101@gmail.com>
2025-03-25 19:08:09 +0600

    Shows menu screen first

--- src/main/java/com/game/CalorieAdventureGame/MainGame.java
+++ src/main/java/com/game/CalorieAdventureGame/MainGame.java
@@ -1,19 +1,11 @@
 package com.game.CalorieAdventureGame;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.game.CalorieAdventureGame.screens.GameScreen;
+import com.game.CalorieAdventureGame.screens.MenuScreen;
 
 public class MainGame extends Game {
     @Override
     public void create() {
-        this.setScreen(new GameScreen(this));  // Set the first screen to GameScreen
-    }
-
-    @Override
-    public void render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  // Clear the screen every frame
-        super.render();  // Render the active screen
+        this.setScreen(new MenuScreen(this));  // Set the first screen to MenuScreen
     }
 }
